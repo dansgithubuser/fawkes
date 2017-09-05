0096fa5626ec64406a10f3ee04c4c5ae2f55cf61
Dan <dansonlinepresence@gmail.com>
2017-09-05 13:48:08 -0400

    run

--- go.py
+++ go.py
@@ -7,8 +7,12 @@ def invoke(invocation):
 import argparse
 parser=argparse.ArgumentParser()
 parser.add_argument('--build', '-b', action='store_true')
+parser.add_argument('--run', '-r', action='store_true')
 args=parser.parse_args()
 
 if args.build:
 	invoke('mix deps.get')
 	invoke('mix deps.compile')
+
+if args.run:
+	invoke('mix phx.server')
