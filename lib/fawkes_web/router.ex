faf25d23f3601e7de26e521bc6d8cd0ec68c88f2
Dan <dansonlinepresence@gmail.com>
2017-09-05 13:49:25 -0400

    add routes

--- lib/fawkes_web/router.ex
+++ lib/fawkes_web/router.ex
@@ -8,4 +8,9 @@ defmodule FawkesWeb.Router do
   scope "/api", FawkesWeb do
     pipe_through :api
   end
+
+  scope "/api", FawkesWeb do
+    post "/create", MainController, :create
+    get "/show", MainController, :show
+  end
 end
