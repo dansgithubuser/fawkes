3cc52a96190f037f90c2dd7b73abaac2c6fac6d7
Dan <dansonlinepresence@gmail.com>
2017-09-05 14:38:46 -0400

    Revert "add routes"
    This reverts commit 3172086b7a848941a6d3fa51bced72738aefc712.

--- lib/fawkes_web/router.ex
+++ lib/fawkes_web/router.ex
@@ -8,9 +8,4 @@ defmodule FawkesWeb.Router do
   scope "/api", FawkesWeb do
     pipe_through :api
   end
-
-  scope "/api", FawkesWeb do
-    post "/create", MainController, :create
-    get "/show", MainController, :show
-  end
 end
