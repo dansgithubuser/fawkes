2fa66449756c71f7a520f8e46b21af560b4559ba
Dan <dansonlinepresence@gmail.com>
2017-09-09 22:44:39 -0400

    change from ets to dets so data lasts longer on heroku

--- lib/fawkes_web/channels/room_channel.ex
+++ lib/fawkes_web/channels/room_channel.ex
@@ -12,18 +12,21 @@ defmodule FawkesWeb.RoomChannel do
     params = Poison.Parser.parse!(params)
     key = params["key"]
     value = params["value"]
-    if :ets.insert_new(:fawkes_table, {key, value}) do
+    {:ok, _} = :dets.open_file(:fawkes_table, [type: :set])
+    if :dets.insert_new(:fawkes_table, {key, value}) do
       Logger.info "associating key #{key} with value #{value}"
     else
       Logger.info "key #{key} already has association"
     end
+    :ok = :dets.close(:fawkes_table)
     {:noreply, socket}
   end
 
   def handle_in("read", params, socket) do
     params = Poison.Parser.parse!(params)
     key = params["key"]
-    value = elem(hd(:ets.lookup(:fawkes_table, key)), 1)
+    {:ok, _} = :dets.open_file(:fawkes_table, [type: :set])
+    value = elem(hd(:dets.lookup(:fawkes_table, key)), 1)
     Logger.info "replying value #{value} for key #{key}"
     {:reply, {:ok, %{"key": key, "value": value}}, socket}
   end
